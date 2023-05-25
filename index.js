fd6a3f1c38a4e208e00b0aa9cdb2a5c8d6a07753
koonx6520 <koonx6520@gmail.com>
2023-05-26 00:38:34 +0700

    add new Gateway Intent

---
+++ index.js
@@ -0,0 +1,55 @@
+import commands from './commands/index.js';
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
+import { config } from 'dotenv';
+
+config();
+
+class Bot extends Client {
+    constructor() {
+        super({
+            intents: [
+                GatewayIntentBits.Guilds,
+                GatewayIntentBits.GuildMembers,
+                GatewayIntentBits.MessageContent,
+                GatewayIntentBits.GuildMessageReactions,
+                GatewayIntentBits.GuildMessages,
+            ],
+        });
+
+        this.commands = new Collection();
+    }
+
+    loadCommands() {
+        for (const command of commands) {
+            this.commands.set(command.data.name, command);
+        }
+    }
+
+    async initialize() {
+        this.loadCommands();
+
+        this.once(Events.ClientReady, (c) => {
+            console.log(`Logged in as ${c.user.tag}`);
+        });
+
+        this.on(Events.InteractionCreate, async (interaction) => {
+            if (!interaction.isChatInputCommand()) return;
+
+            const command = this.commands.get(interaction.commandName);
+
+            if (!command) return;
+
+            try {
+                await command.execute(client, interaction);
+            } catch (error) {
+                console.error(error);
+                // interaction.deleteReply();
+            }
+        });
+
+        this.login(process.env.TOKEN);
+    }
+}
+
+const client = new Bot();
+client.initialize();
