8585efec9bb2529b1553fddabfcf35eb8c9ea9a7
koonx6520 <koonx6520@gmail.com>
2023-05-27 19:36:16 +0700

    add new ui command report

--- commands/index.js
+++ commands/index.js
@@ -1,7 +1,7 @@
 import ping from './info/ping.js';
 import calc from './utils/calc.js';
-import button from './utils/button.js';
+import report from './utils/report.js';
 import dice from './utils/dice.js';
 import role from './roles/role.js';
 
-export default [ping, calc, button, role, dice];
+export default [ping, calc, report, role, dice];
